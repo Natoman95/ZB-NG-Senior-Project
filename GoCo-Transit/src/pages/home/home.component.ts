55af46047d842a9cc82ae70f712e88a473c7202d
Nathan Gray <nathan.gray@gordon.edu>
2018-01-13 16:55:28 -0500

    Modifying file names to match angular naming conventions

--- GoCo-Transit/src/pages/home/home.component.ts
+++ GoCo-Transit/src/pages/home/home.component.ts
@@ -2,10 +2,10 @@ import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
 @Component({
-  selector: 'page-home',
-  templateUrl: 'home.html'
+  selector: 'home-component',
+  templateUrl: 'home.component.html'
 })
-export class HomePage {
+export class HomeComponent {
 
   constructor(public navCtrl: NavController) {
 
