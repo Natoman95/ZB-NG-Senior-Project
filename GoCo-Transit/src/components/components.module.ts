3e870124f5a8b89008033c527a4dbcf6afd6fbac
Nathan Gray <nathan.gray@gordon.edu>
2018-01-18 15:18:52 -0500

    request and offer lists

--- GoCo-Transit/src/components/components.module.ts
+++
@@ -1,9 +0,0 @@
-import { NgModule } from '@angular/core';
-import { FootNavComponent } from './foot-nav/foot-nav';
-
-@NgModule({
-  declarations: [FootNavComponent],
-  imports: [],
-  exports: [FootNavComponent]
-})
-export class ComponentsModule { }
--- GoCo-Transit/src/pages/offers/offers.ts
+++ GoCo-Transit/src/pages/offers/offers.ts
@@ -1,14 +1,6 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { NavController, NavParams } from 'ionic-angular';
 
-/**
- * Generated class for the OffersPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
-
-@IonicPage()
 @Component({
   selector: 'page-offers',
   templateUrl: 'offers.html',
@@ -16,10 +8,6 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 export class OffersPage {
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
-  }
-
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad OffersPage');
   }
 
 }
