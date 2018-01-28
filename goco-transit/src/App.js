3f0063993b53f8e05495e3cdd9772f471d78323e
Nathan Gray <nathan.gray@gordon.edu>
2018-01-27 23:03:32 -0500

    Adding requests page into main page of the app for now

--- goco-transit/src/App.js
+++ goco-transit/src/App.js
@@ -7,9 +7,24 @@ import GroupIcon from 'material-ui-icons/Group';
 import GroupAddIcon from 'material-ui-icons/GroupAdd';
 import FavoriteIcon from 'material-ui-icons/Favorite';
 import SettingsIcon from 'material-ui-icons/Settings';
+import DoneIcon from 'material-ui-icons/Done';
+import DeleteIcon from 'material-ui-icons/Delete';
+import QuestionIcon from 'material-ui-icons/Help';
 import Typography from 'material-ui/Typography';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import createMuiTheme from 'material-ui/styles/createMuiTheme';
+import List, {
+  ListItem,
+  ListItemAvatar,
+  ListItemIcon,
+  ListItemSecondaryAction,
+  ListItemText,
+} from 'material-ui/List';
+import Avatar from 'material-ui/Avatar';
+import IconButton from 'material-ui/IconButton';
+import Button from 'material-ui/Button';
+import AddIcon from 'material-ui-icons/Add';
+import Grid from 'material-ui/Grid';
 
 // Universal color theme
 const theme = createMuiTheme({
@@ -44,9 +59,12 @@ function TabContainer(props) {
   );
 }
 
+// Main page for the application
 class Main extends React.Component {
   state = {
     value: 0,
+    dense: false,
+    secondary: true,
   };
 
   handleChange = (event, value) => {
@@ -59,22 +77,93 @@ class Main extends React.Component {
       // Pass down the theme through the whole app
       <MuiThemeProvider theme={theme}>
         <div>
-          <AppBar position="static" color="primary">
-            <Tabs
-              fullWidth={true}
-              value={this.state.value}
-              onChange={this.handleChange}
-              indicatorColor={theme.palette.primary.light}
-            >
-              <Tab label="Requests" icon={<GroupAddIcon />} />
-              <Tab label="Offers" icon={<GroupIcon />} />
-              <Tab label="Settings" icon={<SettingsIcon />} />
-            </Tabs>
-          </AppBar>
+          <div>
+            <AppBar position="static" color="primary">
+              <Tabs
+                fullWidth={true}
+                value={this.state.value}
+                onChange={this.handleChange}
+                indicatorColor={theme.palette.primary.light}
+              >
+                <Tab label="Requests" icon={<GroupAddIcon />} />
+                <Tab label="Offers" icon={<GroupIcon />} />
+                <Tab label="Settings" icon={<SettingsIcon />} />
+              </Tabs>
+            </AppBar>
+          </div>
 
-          {this.state.value === 0 && <TabContainer>Requests</TabContainer>}
-          {this.state.value === 1 && <TabContainer>Offers</TabContainer>}
-          {this.state.value === 2 && <TabContainer>Settings</TabContainer>}
+          {/* Requests */}
+          <div>
+            {this.state.value === 0 &&
+              <TabContainer>
+                <div>
+                  <List dense={this.state.dense}>
+                    <ListItem button>
+                      <ListItemAvatar>
+                        <Avatar>
+                          <DoneIcon />
+                        </Avatar>
+                      </ListItemAvatar>
+                      <ListItemText
+                        primary="Pittsburgh"
+                        secondary={this.state.secondary ? '12/7-12/19' : null}
+                      />
+                      <ListItemSecondaryAction>
+                        <IconButton aria-label="Delete">
+                          <DeleteIcon />
+                        </IconButton>
+                      </ListItemSecondaryAction>
+                    </ListItem>
+
+                    <ListItem button>
+                      <ListItemAvatar>
+                        <Avatar>
+                          <QuestionIcon />
+                        </Avatar>
+                      </ListItemAvatar>
+                      <ListItemText
+                        primary="Wenham"
+                        secondary={this.state.secondary ? '1/16-18/18' : null}
+                      />
+                      <ListItemSecondaryAction>
+                        <IconButton aria-label="Delete">
+                          <DeleteIcon />
+                        </IconButton>
+                      </ListItemSecondaryAction>
+                    </ListItem>
+                  </List>
+
+                  <Grid container>
+                    <Grid item xs={12}>
+                      <Grid container direction="row" justify="flex-end" alignItems="center">
+                        <Grid item>
+                          <Button fab color="secondary" aria-label="add">
+                            <AddIcon />
+                          </Button>
+                        </Grid>
+                      </Grid>
+                    </Grid>
+                  </Grid>
+
+                </div>
+              </TabContainer>}
+          </div>
+
+          {/* Offers */}
+          <div>
+            {this.state.value === 1 &&
+              <TabContainer>
+
+              </TabContainer>}
+          </div>
+
+          {/* Settings */}
+          <div>
+            {this.state.value === 2 &&
+              <TabContainer>
+
+              </TabContainer>}
+          </div>
         </div>
       </MuiThemeProvider>
     );
