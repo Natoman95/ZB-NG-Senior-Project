e6d634566d201f505790723253a2e700de1ad87a
Nathan Gray <nathan.gray@gordon.edu>
2018-02-13 13:45:30 -0500

    Fixing method comments and service function declarations. Adding http service

---
+++ goco-transit/src/services/http-service.js
@@ -0,0 +1,104 @@
+/**
+ * Handle HTTP requests to the API
+ *
+ * Copied from Gordon 360
+ * 
+ * @module http
+ */
+
+import { createError } from './error';
+import storage from './storage';
+
+const base = process.env.GORDON_360_API_URL;
+
+/**
+ * Make a headers object for use with the API
+ * @description Provides the correct authorization for API requests.
+ * @return {Headers} A headers object
+ */
+const makeHeaders = () => {
+  try {
+    const token = storage.get('token');
+    return new Headers({
+      Authorization: `Bearer ${token}`,
+    });
+  } catch (err) {
+    throw new Error('Token is not available');
+  }
+};
+
+/**
+ * Create a request object with the correct URL and headers for the API
+ * @param {String} url relative URL from base, ex: `activity/023487` (no leading slash)
+ * @param {String} method HTTP method
+ * @param {object|array} body data to send with request
+ * @return {Request} A request object
+ */
+const createRequest = (url, method, body) =>
+  new Request(`${base}api/${url}`, {
+    method,
+    body,
+    headers: makeHeaders(),
+  });
+
+/**
+ * Parse an HTTP response
+ * @param {Response} res HTTP response
+ * @return {Promise.<Object|Array|String|Number>} Resolves with response body; rejects on non-2xx
+ * response code
+ */
+export const parseResponse = res => {
+  // Attempt to parse body of response
+  const json = res
+    .json()
+    // Handle error if response body is not valid JSON
+    .catch(err => Promise.reject(createError(err, res)));
+
+  // Handle error when response body is valid but status code is not
+  if (!res.ok) {
+    return json.then(data => Promise.reject(createError(data, res)));
+  }
+  return json;
+};
+
+/**
+ * Make a request to the API
+ * @param {String} url relative URL from base, ex: `activity/023487` (no leading slash)
+ * @param {String} method HTTP method
+ * @param {object|array} body data to send with request
+ * @return {Promise.<Object>} Response body
+ */
+const makeRequest = (url, method, body) =>
+  fetch(createRequest(url, method, body)).then(parseResponse);
+
+/**
+ * Get
+ * @param {String} url relative URL from base, ex: `activity/023487` (no leading slash)
+ * @return {Promise.<Object>} Response body
+ */
+const get = url => makeRequest(url, 'get');
+
+/**
+ * Put
+ * @param {String} url relative URL from base, ex: `activity/023487` (no leading slash)
+ * @param {object|array} body data to send with request
+ * @return {Promise.<Object>} Response body
+ */
+const put = (url, body) => makeRequest(url, 'put', body);
+
+/**
+ * Post
+ * @param {String} url relative URL from base, ex: `activity/023487` (no leading slash)
+ * @param {object|array} body data to send with request
+ * @return {Promise.<Object>} Response body
+ */
+const post = (url, body) => makeRequest(url, 'post', body);
+
+/**
+ * Delete
+ * @param {String} url relative URL from base, ex: `activity/023487` (no leading slash)
+ * @return {Promise.<Object>} Response body
+ */
+const del = url => makeRequest(url, 'delete');
+
+export default { del, get, post, put };
