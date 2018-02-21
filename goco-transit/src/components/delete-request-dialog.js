5074a5fb667dd758b02df739580607eac450a9a1
Zach Brown <zackattack6991@gmail.com>
2018-02-21 15:57:24 -0500

    Reworked object from const to class

--- goco-transit/src/components/delete-request-dialog.js
+++ goco-transit/src/components/delete-request-dialog.js
@@ -5,28 +5,53 @@ import Dialog, {
     DialogContentText,
     DialogTitle,
   } from 'material-ui/Dialog';
-  import Button from 'material-ui/Button';
+import Button from 'material-ui/Button';
 
-  {/* Delete a request dialog box */}
+{/* Delete a request dialog box */}
 
-  const DeleteRequestDialog = () =>
-  (
-    <Dialog>
-      <DialogTitle id="alert-dialog-title">{"Delete this ride request?"}</DialogTitle>
-      <DialogContent>
-        <DialogContentText id="alert-dialog-description">
-          (Ride data will go here)
-        </DialogContentText>
-      </DialogContent>
-      <DialogActions>
-        <Button onClick={this.handleClose}>
-          Cancel
-        </Button>
-        <Button onClick={this.handleClose}>
-          Delete
-        </Button>
-      </DialogActions>
-    </Dialog>
-  );
+class DeleteRequestDialog extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      dense: false,
+      secondary: true,
+      noGutters: true,
+      divider: true,
+      displayDeleteRequestDialog: false,
+    };
+  }
+
+  // Open the delete request dialog
+  handleClickOpen = () => {
+    this.setState({ displayDeleteRequestDialog: true });
+  };
+
+  // Close the delete request dialog
+  handleClose = () => {
+    this.setState({ displayDeleteRequestDialog: false });
+  };
+
+  render() {
+    return (
+      <Dialog>
+        <DialogTitle id="alert-dialog-title">{"Delete this ride request?"}</DialogTitle>
+        <DialogContent>
+          <DialogContentText id="alert-dialog-description">
+            (Ride data will go here)
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={this.handleClose}>
+            Cancel
+          </Button>
+          <Button onClick={this.handleClose}>
+            Delete
+          </Button>
+        </DialogActions>
+      </Dialog>
+    );
+  }
+}
 
 export default DeleteRequestDialog;
