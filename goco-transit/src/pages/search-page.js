179f921042a64f459a1e97736cd3f5cf28381cdc
Zach Brown <zackattack6991@gmail.com>
2018-02-23 02:06:28 -0500

    Revert "Added time to find a ride search"
    This reverts commit c27db682cd02694299de9522e432bc6b404dcacc.

--- goco-transit/src/pages/search-page.js
+++ goco-transit/src/pages/search-page.js
@@ -135,8 +135,8 @@ class RequestSearchPage extends React.Component {
                   <form noValidate>
                     <TextField
                       id="startDate"
-                      label="Earliest Possible Departure"
-                      type="datetime-local"
+                      label="Earliest Travel Day"
+                      type="date"
                       // Default time for the start date is today
                       defaultValue={this.getFutureDate(0)}
                       value={this.state.startDate}
@@ -156,8 +156,8 @@ class RequestSearchPage extends React.Component {
                   <form noValidate>
                     <TextField
                       id="startDate"
-                      label="Latest Possible Departure"
-                      type="datetime"
+                      label="Latest Travel Day"
+                      type="date"
                       // Default time for the max date is tomorrow
                       defaultValue={this.getFutureDate(1)}
                       onChange={this.handleFormChange('endDate')}
