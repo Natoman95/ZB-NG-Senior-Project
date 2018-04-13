dfc916f418e594aa35a97a9f5ff60b1987a66b9a
Zach Brown <zackattack6991@gmail.com>
2018-04-13 02:43:49 -0400

    Removed dummy data

--- goco-transit/src/services/ride-service.js
+++ goco-transit/src/services/ride-service.js
@@ -23,28 +23,7 @@ import NathanPhoto from '../images/user_profile_nathan.jpg'
  * @return {UserModel} Current user
  */
 const searchOfferedRides = (startDate, endDate, origin, destination) => {
-  // Hardcoded for now - will retrieve from 360
-  let Zach = new UserModel("Zach", "Brown", "zach.brown@gordon.edu");
-  let Nathan = new UserModel("Nathan", "Gray", "nathan.gray@gordon.edu");
-  Nathan.profilePhoto = NathanPhoto;
-  Zach.profilePhoto = ZachPhoto;
-
-  let Rachel = new UserModel("Rachel", "Wonko", "rachel.wonko@gordon.edu");
-  let Jim = new UserModel("Jim", "Bob", "jim.bob@gordon.edu");
-
-  // Dummy rides - will be found in a database somewhere
-  let ride1 = new RideModel(Nathan.username, "Wenham", "Oxford", "2017-12-07", "13:30", 3, "How much more can they take from me? They got my blood, now it's my car!");
-  ride1.passengers = [Rachel];
-  ride1.maxCapacity = 3;
-  ride1.id = "000003";
-
-  let ride2 = new RideModel(Zach.username, "Manchester", "Wenham", "2017-11-03", "08:45", 4, "The way I see it, if you're gonna build a time machine into a car, why not do it with some style?");
-  ride2.passengers = [Jim];
-  ride2.maxCapacity = 4;
-  ride2.id = "000004";
-
-  let rides = [ride1, ride2];
-
+  let rides = null; // Avoids compilation error until this is rewritten 
   return rides;
 }
 
--- goco-transit/src/services/user-service.js
+++ goco-transit/src/services/user-service.js
@@ -30,40 +30,6 @@ const getUser = async () => {
     activeUser.phoneNum = profile.MobilePhone;
     activeUser.profilePhoto = image;
 
-    // Dummy requests
-    let request1 = new RequestModel(activeUser.username, "Wenham", "Pittsburgh", "2017-12-07T08:30", "2017-12-07T20:30", "Take me to church.");
-    let request2 = new RequestModel(activeUser.username, "Manchester", "Wenham", "2018-01-16T13:45", "2018-01-18T10:15", "I call shotgun.");
-
-    activeUser.requests = [
-      request1,
-      request2
-    ];
-
-    // Dummy users for rides and offers
-    let Rachel = new UserModel("Rachel", "Wonko", "rachel.wonko@gordon.edu");
-    let Jim = new UserModel("Jim", "Bob", "jim.bob@gordon.edu");
-
-    // Dummy rides
-    let confirmedRide1 = new RideModel(Jim.username, "Manchester", "Wenham", "2018-04-16", "07:30", 2, "Let's ride.");
-
-    activeUser.confirmedRides = [confirmedRide1];
-
-  // Dummy ride offers
-  let offeredRide1 = new RideModel(activeUser.username, "Boston", "Wenham", "2018-03-08", "18:15", 1); // No note
-  offeredRide1.passengers = [Rachel];
-  offeredRide1.maxCapacity = 4;
-  offeredRide1.id = "000001";
-
-  let offeredRide2 = new RideModel(activeUser.username, "Wenham", "Danvers", "2018-02-18", "22:00", 3, "Cars 2 was the best.");
-  offeredRide2.passengers = [Rachel, Jim];  
-  offeredRide2.maxCapacity = 6;
-  offeredRide2.id = "000002";
-
-    activeUser.offeredRides = [
-      offeredRide1,
-      offeredRide2
-    ];
-
     // Cache user data
     setItem(username, activeUser);
   }
